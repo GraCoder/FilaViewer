0ffde14146f0770c23c31d62fc7b9927de4c4958
t <1753592@outlook.com>
2024-04-13 18:35:26 +0800

    modified:   FilaMat/FilaMat.csproj

---
+++ FilaMat/Views/NativeEmbedView.axaml.cs
@@ -0,0 +1,123 @@
+using System;
+using System.Text;
+using System.Threading.Tasks;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Platform;
+using Avalonia.Interactivity;
+using Avalonia.Markup.Xaml;
+using Avalonia.Platform;
+
+namespace FilaMat.Views
+{
+    public partial class NativeEmbedView : UserControl
+    {
+        public NativeEmbedView()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+
+        public async void ShowPopupDelay(object sender, RoutedEventArgs args)
+        {
+            await Task.Delay(3000);
+            ShowPopup(sender, args);
+        }
+
+        public void ShowPopup(object sender, RoutedEventArgs args)
+        {
+            new ContextMenu()
+            {
+                Items =
+                {
+                    new MenuItem() { Header = "Test" }, new MenuItem() { Header = "Test" }
+                }
+            }.Open((Control)sender);
+        }
+
+        protected override void OnPropertyChanged(AvaloniaPropertyChangedEventArgs change)
+        {
+            base.OnPropertyChanged(change);
+
+            if (change.Property == BoundsProperty)
+            {
+                //var isMobile = change.GetNewValue<Rect>().Width < 1200;
+                //this.Find<DockPanel>("FirstPanel")!.Classes.Set("mobile", isMobile);
+                //this.Find<DockPanel>("SecondPanel")!.Classes.Set("mobile", isMobile);
+            }
+        }
+    }
+
+    public class EmbedView : NativeControlHost
+    {
+        public static INativeControl? Implementation { get; set; }
+
+        static EmbedView()
+        {
+            Implementation = new EmbedSampleWin();
+        }
+
+        protected override IPlatformHandle CreateNativeControlCore(IPlatformHandle parent)
+        {
+            return Implementation?.CreateControl(parent, () => base.CreateNativeControlCore(parent))
+                ?? base.CreateNativeControlCore(parent);
+        }
+
+        protected override void DestroyNativeControlCore(IPlatformHandle control)
+        {
+            base.DestroyNativeControlCore(control);
+        }
+    }
+
+    public interface INativeControl
+    {
+        /// <param name="isSecond">Used to specify which control should be displayed as a demo</param>
+        /// <param name="parent"></param>
+        /// <param name="createDefault"></param>
+        IPlatformHandle CreateControl(IPlatformHandle parent, Func<IPlatformHandle> createDefault);
+    }
+
+
+
+
+
+    public class EmbedSampleWin : INativeControl
+    {
+        private const string RichText =
+            @"{\rtf1\ansi\ansicpg1251\deff0\nouicompat\deflang1049{\fonttbl{\f0\fnil\fcharset0 Calibri;}}
+{\colortbl ;\red255\green0\blue0;\red0\green77\blue187;\red0\green176\blue80;\red155\green0\blue211;\red247\green150\blue70;\red75\green172\blue198;}
+{\*\generator Riched20 6.3.9600}\viewkind4\uc1 
+\pard\sa200\sl276\slmult1\f0\fs22\lang9 <PREFIX>I \i am\i0  a \cf1\b Rich Text \cf0\b0\fs24 control\cf2\fs28 !\cf3\fs32 !\cf4\fs36 !\cf1\fs40 !\cf5\fs44 !\cf6\fs48 !\cf0\fs44\par
+}";
+
+        public IPlatformHandle CreateControl(IPlatformHandle parent, Func<IPlatformHandle> createDefault)
+        {
+            WinApi.LoadLibrary("Msftedit.dll");
+            var handle = WinApi.CreateWindowEx(0, "RICHEDIT50W",
+                @"Rich Edit",
+                0x800000 | 0x10000000 | 0x40000000 | 0x800000 | 0x10000 | 0x0004, 0, 0, 1, 1, parent.Handle,
+                IntPtr.Zero, WinApi.GetModuleHandle(null), IntPtr.Zero);
+            var st = new WinApi.SETTEXTEX { Codepage = 65001, Flags = 0x00000008 };
+            var text = RichText.Replace("<PREFIX>", "");
+            var bytes = Encoding.UTF8.GetBytes(text);
+            WinApi.SendMessage(handle, 0x0400 + 97, ref st, bytes);
+            return new Win32WindowControlHandle(handle, "HWND");
+        }
+    }
+
+    internal class Win32WindowControlHandle : PlatformHandle, INativeControlHostDestroyableControlHandle
+    {
+        public Win32WindowControlHandle(IntPtr handle, string descriptor) : base(handle, descriptor)
+        {
+        }
+
+        public void Destroy()
+        {
+            _ = WinApi.DestroyWindow(Handle);
+        }
+    }
+}
